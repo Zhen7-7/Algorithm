4202bb93ca301ce63bef12f9417d6f5f3c30098d
Zhen77 <Zhen18103690519@gmail.com>
2021-03-10 22:52:00 +0800

    递归

---
+++ Algorithm/src/demo/Recursion.java
@@ -0,0 +1,25 @@
+package demo;
+
+public class Recursion {
+/*
+ * 什么是递归
+ * 递归就是在运行中调用自己
+ * 
+ * */
+	
+	public static void main(String[] args) {
+		int num = recursion(10);
+		System.out.println(num);
+		
+	}
+	static int recursion(int num) {
+		if(num>0) {
+			System.out.println("+"+(num-1));
+		
+			num += recursion(num-1);
+			return num;
+		}else {
+			return num;
+		}
+	}
+}
