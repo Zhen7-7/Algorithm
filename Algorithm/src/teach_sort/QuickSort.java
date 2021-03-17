97ee42e0831e487d1f1acb50337b48e5ddeff7d5
Zhen77 <Zhen18103690519@gmail.com>
2021-03-17 10:46:21 +0800

    CountSort

--- Algorithm/src/teach_sort/QuickSort.java
+++ Algorithm/src/teach_sort/QuickSort.java
@@ -1,11 +1,15 @@
 package teach_sort;
 
 /*
-  	�㷨˼��:
-  	1.�ҵ�һ����(pivot)(��һ��������֮��,�����������ֵȫ��С����,������Ҳಿ��ȫ��������)
-  	2.������ָ����������Ҫ��Ĳ����ƶ�,ֱ��������Υ����Ϊ,��λ�ҽ���
-  	3.�ݹ鷽ʽ:����Ϊ�ֽ粻�ϵĲ��,С������Ȼ��ͬ���ķ�ʽ
-  	4.�����߽�ͼ������
+  	算法思想:
+  	1.找到一个轴(pivot)(第一部分排序之后,会让轴的左侧的值全部小于轴,让轴的右侧部分全部大于轴)
+  	2.左右两指针遇见满足要求的不断移动,直到遇到了违规行为,定位且交换
+  	3.递归方式:以轴为分界不断的拆分,小数组依然用同样的方式
+  	4.处理边界和极端情况
+  	5.平均时间复杂度O(nlogn) 最差O(n²)
+	什么时候最差:本身有顺序:n+(n-1).....
+	如何优化? 判断是否是顺序增长(降低)趋势  或者不仅仅局限在最右侧,每次随机取个数和最右侧交换
+	6.空间复杂度:每次递归需要一些额外的变量的空间,取决于分了多少层O(logN)
  
  */
 public class QuickSort {
@@ -21,7 +25,7 @@ public class QuickSort {
 		sort(arr,leftBound,mid-1);
 		sort(arr,mid+1,rightBound);
 	}
-	//����
+	//分区
 	static int partition(int[] arr,int leftBound,int rightBound) {
 		int left = leftBound;
 		int right = rightBound-1;
